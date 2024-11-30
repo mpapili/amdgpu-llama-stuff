45df28133715d34beb5e0bf40dcb8c7b6d0e8e5c
Mike <mpapili@ramapo.edu>
2024-11-30 14:59:54 -0500

    Update install_prereqs.sh

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+# NOTE - I suspect that I had rocm previously installed, this may be incomplete?
 sudo apt install -y libamd-comgr2 libamdhip64-5 libhipblas0 libhipfft0 \
   libhiprand1 libhipsolver0 libhipsparse0 libhsa-runtime64-1 librccl1 \
   librocalution0 librocblas0 librocfft0 librocm-smi64-1 librocrand1 \
