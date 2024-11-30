2508aaceb8b2b7312c3251278e158c4df58b1c71
Mike <mpapili@ramapo.edu>
2024-11-30 14:59:35 -0500

    Update install_prereqs.sh

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-sudo apt install -y libamd-comgr2 libamdhip64-5 libhipblas0 libhipfft0 libhiprand1 libhipsolver0 libhipsparse0 libhsa-runtime64-1 librccl1 librocalution0 librocblas0 librocfft0 librocm-smi64-1 librocrand1 librocsolver0 librocsparse0 rocm-device-libs-17 rocm-smi rocminfo
+sudo apt install -y libamd-comgr2 libamdhip64-5 libhipblas0 libhipfft0 \
+  libhiprand1 libhipsolver0 libhipsparse0 libhsa-runtime64-1 librccl1 \
+  librocalution0 librocblas0 librocfft0 librocm-smi64-1 librocrand1 \
+  librocsolver0 librocsparse0 rocm-device-libs-17 rocm-smi rocminfo \
+  cmake
