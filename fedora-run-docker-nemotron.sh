1e2c89393eabcd6c86d302dcf27bea59df433d2c
mpapili <michaelpapili@gmail.com>
2025-12-18 22:34:18 -0500

    lots of random runners and dockerfiles I've been using

---
+++ fedora-run-docker-nemotron.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+docker run --rm \
+    --name llama-rocm \
+    --device=/dev/kfd \
+    --device=/dev/dri/renderD128 \
+    --device=/dev/dri/renderD129 \
+    --cap-drop=ALL \
+    -v /home/mike/Downloads/LLMs:/models:Z \
+    --user $(id -u):$(id -g) \
+    --security-opt no-new-privileges \
+    -p 0.0.0.0:8080:8080 \
+    -it fedora-llama-cpp-nemotron:latest \
+    /bin/bash
